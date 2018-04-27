44d05480573a77de0533494b06290b605c1099eb
gj812 <gja@goplex.co.uk>
2018-04-27 08:23:53 +0100

    Initial comit working with routing

---
+++ AtTheMovies/Client/Scripts/DetailsController.js
@@ -0,0 +1,20 @@
+﻿
+(function (app) {
+  var DetailsController = function ($scope, $http, $routeParams) {
+    var id = $routeParams.id;
+
+    $http.get('api/movies/' + id).then(successCallback, errorCallback);
+    
+
+  function successCallback(response) {
+    $scope.movie = response.data
+  }
+  function errorCallback(error) {
+    console.log("Error getting movie " + id);
+  }
+
+  };
+  app.controller("DetailsController", DetailsController);
+
+
+}(angular.module("atTheMovies")));
---
+++ AtTheMovies/Client/Scripts/ListController.js
@@ -0,0 +1,31 @@
+﻿
+(function (app) {
+
+  var ListController = function ($scope, $http) {
+    
+    $http({
+      method: 'GET',
+      url: '/api/movies'
+    }).then(function (response) {
+      $scope.movies = response.data;
+      console.log(response.data);
+      }, function (error) {
+        console.log("Error listing movies");
+      });
+  };
+  //ListController.$inject = ["$scope", "$http"];
+
+  app.controller("ListController", ListController);
+
+}(angular.module("atTheMovies")));
+
+
+
+/*
+I could also write this like so:
+
+(function (app) {
+  var app = angular.module("atTheMovies");
+}());
+
+*/
